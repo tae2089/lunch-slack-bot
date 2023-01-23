98e84b2b9ae3d940190167a0b4f9c583368ca1c0
tae2089 <tae2089002@gmail.com>
2023-01-23 18:09:26 +0900

    userName 제거

--- internal/config/db_test.go
+++ internal/config/db_test.go
@@ -4,17 +4,17 @@ import (
 	"bc-labs-lunch-bot/ent/enttest"
 	"context"
 	"fmt"
-	"github.com/go-playground/assert/v2"
-	_ "github.com/mattn/go-sqlite3"
 	"testing"
 	"time"
+
+	"github.com/go-playground/assert/v2"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func TestOpenDB(t *testing.T) {
 	client := enttest.Open(t, "sqlite3", "file:ent?mode=memory&_fk=1")
 	defer client.Close()
 	lunch, err := client.Lunch.Create().
-		SetUserName("asd").
 		SetCafeName("asd").
 		SetRestaurantName("asd").
 		SetPayer("asd").
